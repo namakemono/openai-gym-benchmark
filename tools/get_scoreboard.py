3a8282f4073bdf8f8d2951e9311b3806025a79c3
namakemono <viola.hirtipes@gmail.com>
2017-08-05 09:14:01 +0900

    Fetch success scores from all problems.

--- tools/get_scoreboard.py
+++ tools/get_scoreboard.py
@@ -1,18 +1,58 @@
-import lxml.html
+import logging 
+logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-def run():
-    root = lxml.html.parse("/tmp/CartPole-v0.html").getroot()
+import pandas as pd
+import requests
+import lxml.html
+from gym.envs.registration import registry
+
+BASE_URL = "https://gym.openai.com"
+
+def run(fpath):
+    df_all = None
+    for env_id in registry.env_specs.keys():
+        logger.info("Downloading: `env_id=%s`" % env_id)
+        r = requests.get("%s/envs/%s" % (BASE_URL, env_id))
+        if r.status_code == 200:
+            records = parse(html=r.text)
+            if len(records) > 0:
+                df = pd.DataFrame(records)
+                df["env_id"] = env_id
+                df_all = pd.concat([df_all, df])
+                df_all.to_csv(fpath, index=False, encoding="utf-8")
+        else:
+            logger.info("[%s Error] Download failed for `env_id=%s`" % (r.status_code, env_id))
+
+def test():
+    html = "\n".join(open("/tmp/CartPole-v0.html").readlines())
+    df = pd.DataFrame(parse(html))
+    print df
+
+def parse(html):
+    records = []
+    root = lxml.html.fromstring(html) # .getroot()
     for tr in root.xpath("//tr[@class='success']"):
         record = {}
         for i, td in enumerate(tr.xpath("./td")):
-            if (i == 0) and ("writeup" in td.xpath("normalize-space(./span)")):
-                a = td.xpath("./a")[0]
-                record["url"] = a.get("href")
-                record["username"] = a.text.strip().replace("'s algorithm", "")
-            if (i == 1):
+            if (i == 0):
+                if ("writeup" in td.xpath("normalize-space(./span)")):
+                    a = td.xpath("./a")[0]
+                    record["url"] = "%s%s" % (BASE_URL, a.get("href"))
+                    record["username"] = a.text.strip().replace("'s algorithm", "")
+            elif (i == 1):
                 record["score"] = td.xpath("./a/text()")[0].strip()
+            elif (i == 2):
+                record["timestamp"] = td.xpath("./a/span[@class='timestamp']")[0].get("title")
         if "username" in record:
-            print record
+            logger.debug(record)
+            records.append(record)
+    return records
 
 if __name__ == "__main__":
-    run()
+    test()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fpath', type=str, default="/tmp/openai_gym_scoreboard.csv")
+    args = parser.parse_args()
+    run(args.fpath)
