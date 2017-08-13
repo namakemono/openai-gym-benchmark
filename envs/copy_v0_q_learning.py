000d32a26955cdb08756829fdc69d95eb518bd15
namakemono <viola.hirtipes@gmail.com>
2017-08-13 22:40:13 +0900

    Modify Q-Learning of Copy-v0.

--- envs/copy_v0_q_learning.py
+++ envs/copy_v0_q_learning.py
@@ -9,24 +9,29 @@ def run(alpha=0.3, gamma=0.9):
     Gs = []
     for episode in range(10**6):
         x = env.reset()
-        G = 0
+        X, A, R = [], [], [] # States, Actions, Rewards
         done = False
         while not done:
             if (np.random.random() < 0.01) or (not x in Q):
                 a = env.action_space.sample()
             else:
                 a = sorted(Q[x].items(), key=lambda _: -_[1])[0][0]
-            nx, r, done, _ = env.step(a)
+            X.append(x)
+            A.append(a)
             if not x in Q:
-                Q[x] = {a: 0}
-            if not nx in Q:
-                Q[nx] = {a: 0}
+                Q[x] = {}
             if not a in Q[x]:
                 Q[x][a] = 0
-            G += r
+            x, r, done, _ = env.step(a)
+            R.append(r)
+        T = len(X)
+        x, a, r = X[-1], A[-1], R[-1]
+        Q[x][a] += alpha * (r - Q[x][a])
+        for t in range(T-2, -1, -1):
+            x, nx, a, r = X[t], X[t+1], A[t], R[t]
             Q[x][a] += alpha * (r + gamma * np.max(Q[nx].values()) - Q[x][a])
-            x = nx
-        print "Episode: %d, Reward: %d" % (episode, G)
+        G = sum(R) # Revenue 
+        print "Episode: %d, Revenue: %d" % (episode, G)
         Gs.append(G)
         if np.mean(Gs[-100:]) > 25.0:
             break
